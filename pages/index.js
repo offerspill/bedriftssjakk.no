bd91725f0b885836cf35f3f5f67d40bbb419fe0c
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-12-29 17:23:35 +0100

    Add cols

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
 import Navbar from "../components/Navbar/Navbar";
 import { useState } from "react";
+import Image from "next/image";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
+`;
+
+const FrontpageContainer = styled.div`
+  box-sizing: border-box;
+  background: #2632a3;
+  height: 100vh;
+  padding: 3rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FrontpageImage = styled(Image)`
+  width: 400px;
+  height: 100px;
+`;
+
+const Column1 = styled.div`
+  width: 50%;
+  height: 50%;
+`;
+
+const Column2 = styled.div`
+  width: 50%;
+  height: 50%;
 `;
 
 export default function Home() {
@@ -14,11 +41,25 @@ export default function Home() {
     setIsOpen(!isOpen);
   };
 
-  const dummyNavbar = [{ link: "https://vg.no", title: "VG" }];
+  const dummyNavbar = [
+    { link: "https://vg.no", title: "Hjem" },
+    { link: "https://vg.no", title: "Våre pakker" },
+    { link: "https://vg.no", title: "Instruktører" },
+    { link: "https://vg.no", title: "Kontakt" },
+  ];
 
   return (
     <>
       <Navbar toggle={toggle} navbar={dummyNavbar} />
+      <FrontpageContainer>
+        <Column1>
+          <h1>Sjakk for bedrifter</h1>
+          <p>Hjemmekontor?</p>
+        </Column1>
+        <Column2>
+          <img width="500px" src="./chess.jpeg" />
+        </Column2>
+      </FrontpageContainer>
     </>
   );
 }
