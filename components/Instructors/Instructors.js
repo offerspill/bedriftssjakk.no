0bf87022a0f16fd3d014a8d8a439b16e490ef755
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-01-03 21:45:58 +0100

    Add instructors

--- components/Instructors/Instructors.js
+++ components/Instructors/Instructors.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import styled from "styled-components";
 
 const StyledInstructors = styled.div`
@@ -9,22 +9,99 @@ const StyledInstructors = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+
+  padding: 1rem;
 
   h1 {
     display: block;
     text-align: center;
+
+    @media screen and (max-width: 1200px) {
+      margin-top: 2rem;
+    }
   }
 `;
 
-const InstructorContent = styled.div``;
+const Name = styled.span`
+  color: black;
+
+  font-size: 20px;
+  margin-top: 0.5rem;
+  text-align: center;
+`;
+
+const Instructor = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background: white;
+  padding: 2rem;
+  border-radius: 5px;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  max-width: 350px;
+`;
+
+const Image = styled.img`
+  clip-path: circle();
+  margin: 0 auto;
+`;
+
+const Title = styled.span`
+  color: #615e5e;
+  font-size: 18px;
+  text-align: center;
+`;
+
+const Description = styled.p`
+  font-size: 16px;
+  text-align: center;
+  color: #3d3c3c;
+  margin-top: 1rem;
+`;
+
+const InstructorContent = styled.div`
+  display: flex;
+  flex-basis: auto;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 const Instructors = () => {
   return (
     <StyledInstructors id="instruktorer">
+      <Heading as="h1" size="3xl" mb="2rem">
+        Instruktører
+      </Heading>
       <InstructorContent>
-        <Heading as="h1" size="3xl" mb="2rem">
-          Instruktører
-        </Heading>
+        <Instructor>
+          <div>
+            <Image src="./testimg.jpg" />
+          </div>
+          <Name>Navn navn navn</Name>
+          <Title>Candidate master</Title>
+          <Description>Blablabla er en internasjonal maeste rsom</Description>
+        </Instructor>
+        <Instructor>
+          <div>
+            <Image src="./testimg.jpg" />
+          </div>
+          <Name>Navn navn navn</Name>
+          <Title>Stormester</Title>
+          <Description>Blablabla er en internasjonal maeste rsom</Description>
+        </Instructor>
+        <Instructor>
+          <div>
+            <Image src="./testimg.jpg" />
+          </div>
+          <Name>Navn navn navn</Name>
+          <Title>Internasjonal mester</Title>
+          <Description>
+            Blablabla bøabøabøa babøabøabøabø fiorjoer jgore gjreo greijer
+          </Description>
+        </Instructor>
       </InstructorContent>
     </StyledInstructors>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
   },
   plugins: [],
-}
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
